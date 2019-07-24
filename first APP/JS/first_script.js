4d7eb1f95be8393a3c750f44f9f5f5dac2e05539
kaouther9 <fekhari39@gmail.com>
2019-07-24 21:44:45 +0100

    Resetting the calculator

--- first APP/JS/first_script.js
+++ first APP/JS/first_script.js
@@ -4,6 +4,13 @@ const calculator = {
   secundoperand: false,
   operation: null, //the operator for an expression
 };
+const performecalculation = {
+  '/': (leftoperand, secundoperand) => leftoperand / secundoperand,
+  '*': (leftoperand, secundoperand) => leftoperand * secundoperand,
+  '+': (leftoperand, secundoperand) => leftoperand + secundoperand,
+  '-': (leftoperand, secundoperand) => leftoperand - secundoperand,
+  '=': (leftoperand, secundoperand) => secundoperand
+};
 
 function updatedisplay() {
   const display = document.querySelector(".screen-Calculator");
@@ -21,16 +28,18 @@ Keys.addEventListener('click', (event) => {
     return;
   }
   if (target.classList.contains('operator')) {
-    console.log('operator', target.value);
+    Oparator(target.value);
+    updatedisplay();
     return;
   }
   if (target.classList.contains('dote')) {
-    inputDecimal(target.value);
-    updateDisplay();
+    inputDot(target.value);
+    updatedisplay();
     return;
   }
   if (target.classList.contains('clear')) {
-    console.log('clear', target.value);
+    clear(target.value);
+    updatedisplay();
     return;
   }
   inputDigit(target.value);
@@ -40,14 +49,53 @@ Keys.addEventListener('click', (event) => {
 
 function inputDigit(digit) {
   const {
-    displayValue
+    displayValue,
+    secundoperand
   } = calculator;
-  calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+  if (secundoperand === true) {
+    calculator.displayValue = digit;
+    calculator.secundoperand = false;
+  } else {
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+  }
 
+  console.log(calculator);
 }
 
 function inputDot(dot) {
-  if (!calculator.displayValue.include(dot)) {
+  if (!calculator.displayValue.includes(dot)) {
     calculator.displayValue += dot;
   }
+}
+
+function Oparator(nextoparator) {
+  const {
+    leftoperand,
+    displayValue,
+    operation
+  } = calculator
+  const inputValue = parseFloat(displayValue);
+  if (operation && calculator.secundoperand) {
+    calculator.operation = nextoparator;
+    console.log(calculator);
+    return;
+  }
+  if (leftoperand == null) {
+    calculator.leftoperand = inputValue;
+  } else if (operation) {
+    const curentValue = leftoperand || 0;
+    const result = performecalculation[operation](leftoperand, inputValue);
+    calculator.displayValue = String(result);
+    calculator.leftoperand = result;
+  }
+  calculator.secundoperand = true;
+  calculator.operation = nextoparator;
+}
+
+function clear() {
+  calculator.displayValue = '0';
+  calculator.leftoperand = null;
+  calculator.secundoperand = false;
+  calculator.operation = null;
+  console.log(calculator);
 }
